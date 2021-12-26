793c09e4f9ffa99a64d934d48676d8a09625aa3a
yujeong jang <aridoasis@naver.com>
2021-12-26 11:22:04 +0900

    JSP_web

---
+++ web/src/main/java/com/yedam/web/impl/JobsMapper.java
@@ -0,0 +1,11 @@
+package com.yedam.web.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobsMapper {
+List<Map> getJobs();
+}
